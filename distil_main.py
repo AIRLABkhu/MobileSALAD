69f145c50540738e90e939f53d1c1422c7729f89
Yu Geunhyeok <nda111>
2024-02-23 16:54:42 +0900

    On branch distil Changes to be committed: 	new file:   distil_main.py 	new file:   distil_vpr_model.py 	modified:   mod_main.py

--- distil_main.py
+++ distil_main.py
@@ -5,7 +5,7 @@ import pytorch_lightning.callbacks as pl_callbacks
 from pytorch_lightning.strategies import DeepSpeedStrategy
 from pytorch_lightning import strategies
 
-from mod_vpr_model import VPRModel
+from dyn_vpr_model import VPRModel
 from dataloaders.GSVCitiesDataloader import GSVCitiesDataModule
 
 from argparse import ArgumentParser
@@ -25,13 +25,12 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=int, default=0)
     parser.add_argument('--tag', type=str, required=True)
 
-    parser.add_argument('--masking-rate', type=float, default=0.2)
-    parser.add_argument('--val-masking-rate', type=float, default=None)
-
-    parser.add_argument('--masking-mode', type=str, default=None)
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--check-val', type=int, default=1)
     parser.add_argument('--lr', type=float, default=6e-6)
+
+    parser.add_argument('--masking-ratio', type=float, default=0.2)
+    parser.add_argument('--prun-loc', type=int, nargs='+', default=[8,9,10])
 
     # parser.add_argument('--trainable_blocks', default=)
 
@@ -44,8 +43,8 @@ if __name__ == '__main__':
         shuffle_all=False, # shuffle all images or keep shuffling in-city only
         random_sample_from_each_place=True,
         image_size=(224, 224),
-        num_workers=8,
-        persistent_workers=True, # for CPU
+        num_workers=2,
+        pin_memory=True,
         show_data_stats=True,
         val_set_names=[
             # 'pitts30k_val', 
@@ -63,13 +62,19 @@ if __name__ == '__main__':
         #---- Encoder
         backbone_arch=backbone_arch,
         backbone_config={
-            'model_name': 'dinov2_vitb14', 
-            'num_trainable_blocks': [8,9,10], 
-            'return_token': True, 
-            'norm_layer': True, 
-            'masking_rate': args.masking_rate,  #
-            'val_masking_rate': args.val_masking_rate,  
-            'masking_mode': args.masking_mode,  
+            'model_name': backbone_arch,
+            'num_trainable_blocks': args.prun_loc,
+            'return_token': True,
+            'norm_layer': True,
+            'masking_ratio': args.masking_ratio,
+        },
+
+        teacher_arch = backbone_arch,
+        teacher_config={
+            'model_name' : backbone_arch,
+            'return_token' : True,
+            'norm_layer': True,
+            'num_trainable_blocks' : 0,
         },
         
         agg_arch='SALAD',
@@ -100,34 +105,6 @@ if __name__ == '__main__':
         faiss_device=args.device
     )
 
-    state_dict = torch.load('weights/dino_salad.ckpt', map_location='cpu')
-    # state_dict['backbone.predictor.in_conv.0.weight'] = model.backbone.predictor.in_conv[0].weight.data.cpu()
-    # state_dict['backbone.predictor.in_conv.0.bias'] = model.backbone.predictor.in_conv[0].bias.data.cpu()
-    # state_dict['backbone.selector.in_conv.0.weight'] = model.backbone.selector.in_conv[0].weight.data.cpu()
-
-    for i, selector in enumerate(model.backbone.selectors):
-        # state_dict[f'backbone.selectors.{i}.in_conv.0.weight'] = selector.in_conv[0].weight.data.cpu()
-        # state_dict[f'backbone.selectors.{i}.in_conv.0.bias'] = selector.in_conv[0].bias.data.cpu()
-
-        state_dict[f'backbone.selectors.{i}.fc1.weight'] = selector.fc1.weight.data.cpu()
-        state_dict[f'backbone.selectors.{i}.fc1.bias'] = selector.fc1.bias.data.cpu()
-        state_dict[f'backbone.selectors.{i}.fc2.weight'] = selector.fc2.weight.data.cpu()
-        state_dict[f'backbone.selectors.{i}.fc2.bias'] = selector.fc2.bias.data.cpu()
-
-        # dynamic vit selector
-        # for k, in_selector in enumerate(selector.in_conv):
-        #     if 'weight' not in dir(in_selector):
-        #         continue
-        #     state_dict[f'backbone.selectors.{i}.in_conv.{k}.weight'] = in_selector.weight.data.cpu()
-        #     state_dict[f'backbone.selectors.{i}.in_conv.{k}.bias'] = in_selector.bias.data.cpu()
-        # for k, out_selector in enumerate(selector.out_conv):
-        #     if 'weight' not in dir(out_selector):
-        #         continue
-        #     state_dict[f'backbone.selectors.{i}.out_conv.{k}.weight'] = out_selector.weight.data.cpu()
-        #     state_dict[f'backbone.selectors.{i}.out_conv.{k}.bias'] = out_selector.bias.data.cpu()
-
-
-    model.load_state_dict(state_dict)
 
     # model params saving using Pytorch Lightning
     # we save the best 3 models accoring to Recall@1 on pittsburg val
@@ -141,30 +118,13 @@ if __name__ == '__main__':
         mode='max'
     )
 
-    #------------------
-    # we instanciate a trainer
-    # trainer = pl.Trainer(
-    #     accelerator='gpu',
-    #     devices=4, strategy='ddp_find_unused_parameters_true',
-    #     default_root_dir=f'./logs/', # Tensorflow can be used to viz 
-    #     num_nodes=1,
-    #     num_sanity_val_steps=0, # runs a validation step before stating training
-    #     precision='16-mixed', # we use half precision to reduce  memory usage
-    #     max_epochs=10,  # increased by 8 because the batch was halved. 
-    #     check_val_every_n_epoch=1, # run validation every epoch
-    #     callbacks=[checkpoint_cb],# we only run the checkpointing callback (you can add more)
-    #     reload_dataloaders_every_n_epochs=1, # we reload the dataset to shuffle the order
-    #     log_every_n_steps=20,
-    # )
+
     trainer = pl.Trainer(
         accelerator='gpu', 
-        # strategy='ddp_find_unused_parameters_true',
         devices=[args.device],
         default_root_dir=f'./logs/{args.tag}', # Tensorflow can be used to viz 
         num_nodes=1,
         num_sanity_val_steps=0, # runs a validation step before stating training
-        # precision='16-mixed', # we use half precision to reduce memory usage
-        # precision='32', # we use half precision to reduce memory usage
         max_epochs=args.epochs,  # increased by 8 because the batch was halved. 
         check_val_every_n_epoch=args.check_val, # run validation every epoch
         callbacks=[checkpoint_cb],# we only run the checkpointing callback (you can add more)
@@ -175,3 +135,4 @@ if __name__ == '__main__':
 
     # we call the trainer, we give it the model and the datamodule
     trainer.fit(model=model, datamodule=datamodule)
+    # trainer.validate(model=model, datamodule=datamodule)
